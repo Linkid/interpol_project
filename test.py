8dfe68fd5f2e22049cc51f4b157e6935a27fd53d
François Magimel <francois.magimel@etu.enseeiht.fr>
2014-03-31 00:02:46 +0200

    Tests: added every vertices and faces of the regular octohedron and tests results modified

--- test.py
+++ test.py
@@ -10,24 +10,58 @@ from proj_interpol2A import CatmullClark
 class TestEvalution(unittest.TestCase):
     def setUp(self):
         ## init the mesh
-        self.vertices = [[1, 0, 0], [0, 0, 1], [0, -1, 0]]
-        self.faces = [[1, 5, 4]]
+        self.vertices = [[1, 0, 0],
+                         [0, 1, 0],
+                         [-1, 0, 0],
+                         [0, -1, 0],
+                         [0, 0, 1],
+                         [0, 0, -1]]
+        self.faces = [[0, 4, 3],
+                      [3, 4, 2],
+                      [2, 4, 1],
+                      [1, 4, 0],
+                      [0, 3, 5],
+                      [3, 2, 5],
+                      [2, 1, 5],
+                      [1, 0, 5]]
         self.mesh = {'vertices': self.vertices, 'faces': self.faces}
 
         ## create the CatmullClark object
         self.cat = CatmullClark(self.mesh)
 
     def test_face_point(self):
-        face_point = self.cat.face_point(self.vertices)
+        vertices = [self.vertices[v] for v in self.faces[0]]
+        #print(vertices)
+        face_point = self.cat.face_point(vertices)
         np_test.assert_array_equal(face_point, np.array([1/3., -1/3., 1/3.]))
 
     def test_edges_vertex(self):
+        # Faces of vertices to edges of vertices
         self.cat.edges_vertex()
-        self.assertEqual(self.cat.edges_v, {2: (1, 4), 1: (1, 5), 0: (4, 5)})
+        self.assertEqual(self.cat.edges_v, {0: (1, 2),
+                                            1: (0, 1),
+                                            2: (2, 5),
+                                            3: (1, 4),
+                                            4: (1, 5),
+                                            5: (2, 3),
+                                            6: (0, 4),
+                                            7: (0, 5),
+                                            8: (0, 3),
+                                            9: (3, 4),
+                                            10: (2, 4),
+                                            11: (3, 5)})
 
     def test_faces_edge(self):
+        # Faces of vertices to faces of edges
         self.cat.faces_edge()
-        self.assertEqual(self.cat.faces_e, {0: [0, 1, 2]})
+        self.assertEqual(self.cat.faces_e, {0: [6, 8, 9],
+                                            1: [5, 9, 10],
+                                            2: [0, 3, 10],
+                                            3: [1, 3, 6],
+                                            4: [7, 8, 11],
+                                            5: [2, 5, 11],
+                                            6: [0, 2, 4],
+                                            7: [1, 4, 7]})
 
 
 if __name__ == '__main__':
