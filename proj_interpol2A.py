53e5abac896a3f484a3d05e72920b5979c519252
François Magimel <francois.magimel@etu.enseeiht.fr>
2014-03-30 20:22:06 +0200

    Useless imports commented (scipy, matplotlib and Tkinter)

--- proj_interpol2A.py
+++ proj_interpol2A.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 
-import matplotlib
-matplotlib.use('TkAgg')
+#import matplotlib
+#matplotlib.use('TkAgg')
 
 import numpy as np
-import scipy as sp
-import matplotlib.pyplot as plt
-import matplotlib.backends.backend_tkagg as pdfagg
-import Tkinter as Tk
+#import scipy as sp
+#import matplotlib.pyplot as plt
+#import matplotlib.backends.backend_tkagg as pdfagg
+#import Tkinter as Tk
 
 from itertools import combinations
 
@@ -83,12 +83,12 @@ class CatmullClark:
 ## face
 
 
-plt.ion()
-
-if __name__ == '__main__':
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-
-
-plt.ioff()
-plt.show()
+#plt.ion()
+#
+#if __name__ == '__main__':
+#    fig = plt.figure()
+#    ax = fig.add_subplot(111)
+#
+#
+#plt.ioff()
+#plt.show()
