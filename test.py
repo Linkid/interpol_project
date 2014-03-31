82dd4588f5667df3dfe44bb29805da1ae6661012
François Magimel <francois.magimel@etu.enseeiht.fr>
2014-03-31 17:03:30 +0200

    Tests: indices must begin at 0, not 1…

--- test.py
+++ test.py
@@ -18,12 +18,12 @@ class TestEvalution(unittest.TestCase):
                          [0.5, -0.5, -0.5],
                          [0.5, 0.5, -0.5],
                          [0.5, 0.5, 0.5]]
-        self.faces = [[4, 3, 2, 1],
+        self.faces = [[3, 2, 1, 0],
+                      [1, 5, 4, 0],
                       [2, 6, 5, 1],
-                      [3, 7, 6, 2],
-                      [8, 7, 3, 4],
-                      [5, 8, 4, 1],
-                      [6, 7, 8, 5]]
+                      [7, 6, 2, 3],
+                      [4, 7, 3, 0],
+                      [5, 6, 7, 4]]
         self.mesh = {'vertices': self.vertices, 'faces': self.faces}
 
         ## create the CatmullClark object
@@ -32,45 +32,45 @@ class TestEvalution(unittest.TestCase):
     def test_face_point(self):
         vertices = [self.vertices[v] for v in self.faces[0]]
         face_point = self.cat.face_point(vertices)
-        np_test.assert_array_equal(face_point, np.array([-1/4., 0., 0.]))
+        np_test.assert_array_equal(face_point, np.array([-1/2., 0., 0.]))
 
     def test_edges_vertex(self):
         # Faces of vertices to edges of vertices
         self.cat.edges_vertex()
         self.assertEqual(self.cat.edges_v, {0: (4, 7),
                                             1: (1, 3),
-                                            2: (4, 8),
-                                            3: (5, 6),
+                                            2: (5, 6),
+                                            3: (0, 7),
                                             4: (1, 6),
                                             5: (3, 7),
                                             6: (2, 5),
-                                            7: (5, 8),
+                                            7: (0, 3),
                                             8: (1, 2),
                                             9: (6, 7),
                                             10: (1, 5),
                                             11: (3, 6),
-                                            12: (2, 6),
-                                            13: (4, 5),
-                                            14: (1, 4),
-                                            15: (2, 3),
-                                            16: (2, 7),
-                                            17: (6, 8),
-                                            18: (3, 4),
-                                            19: (5, 7),
-                                            20: (3, 8),
-                                            21: (1, 8),
-                                            22: (7, 8),
-                                            23: (2, 4)})
+                                            12: (0, 4),
+                                            13: (2, 7),
+                                            14: (2, 6),
+                                            15: (4, 5),
+                                            16: (1, 4),
+                                            17: (2, 3),
+                                            18: (0, 1),
+                                            19: (4, 6),
+                                            20: (5, 7),
+                                            21: (0, 5),
+                                            22: (3, 4),
+                                            23: (0, 2)})
 
     def test_faces_edge(self):
         # Faces of vertices to faces of edges
         self.cat.faces_edge()
-        self.assertEqual(self.cat.faces_e, {0: [1, 8, 14, 15, 18, 23],
-                                            1: [3, 4, 6, 8, 10, 12],
-                                            2: [5, 9, 11, 12, 15, 16],
-                                            3: [0, 2, 5, 18, 20, 22],
-                                            4: [2, 7, 10, 13, 14, 21],
-                                            5: [3, 7, 9, 17, 19, 22]})
+        self.assertEqual(self.cat.faces_e, {0: [1, 7, 8, 17, 18, 23],
+                                            1: [10, 12, 15, 16, 18, 21],
+                                            2: [2, 4, 6, 8, 10, 14],
+                                            3: [5, 9, 11, 13, 14, 17],
+                                            4: [0, 3, 5, 7, 12, 22],
+                                            5: [0, 2, 9, 15, 19, 20]})
 
     def test_edge_point(self):
         id_edge = 8  # vertices (0, 1)
@@ -80,17 +80,37 @@ class TestEvalution(unittest.TestCase):
         vertices = [self.vertices[v] for v in self.faces[0]]
         face_points.append(self.cat.face_point(vertices))
         np_test.assert_array_equal(face_points[-1],
-                                   np.array([-1/4., 0., 0.]))
+                                   np.array([-1/2., 0., 0.]))
         # add the face point of the face 1
         vertices = [self.vertices[v] for v in self.faces[1]]
         face_points.append(self.cat.face_point(vertices))
         np_test.assert_array_equal(face_points[-1],
+                                   np.array([0., -1/2., 0.]))
+        # add the face point of the face 2
+        vertices = [self.vertices[v] for v in self.faces[2]]
+        face_points.append(self.cat.face_point(vertices))
+        np_test.assert_array_equal(face_points[-1],
                                    np.array([0., 0., -1/2.]))
+        # add the face point of the face 3
+        vertices = [self.vertices[v] for v in self.faces[3]]
+        face_points.append(self.cat.face_point(vertices))
+        np_test.assert_array_equal(face_points[-1],
+                                   np.array([0., 1/2., 0.]))
+        # add the face point of the face 4
+        vertices = [self.vertices[v] for v in self.faces[4]]
+        face_points.append(self.cat.face_point(vertices))
+        np_test.assert_array_equal(face_points[-1],
+                                   np.array([0., 0., 1/2.]))
+        # add the face point of the face 5
+        vertices = [self.vertices[v] for v in self.faces[5]]
+        face_points.append(self.cat.face_point(vertices))
+        np_test.assert_array_equal(face_points[-1],
+                                   np.array([1/2., 0., 0.]))
 
         edge_point = self.cat.edge_point(id_edge, face_points)
 
         np_test.assert_allclose(edge_point,
-                                np.array([-5/16., 0., -3/8.]))
+                                np.array([-3/8., 0., -3/8.]))
 
 
 if __name__ == '__main__':
